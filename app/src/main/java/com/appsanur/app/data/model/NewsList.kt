d3bbfe09e24f81442d337359395f6b075ce5e46c
Ikhsan Nur Fatah <75824971+IkhsanNurF@users.noreply.github.com>
2021-01-17 06:51:18 +0700

    Delete app/src directory

--- app/src/main/java/com/appsanur/app/data/model/NewsList.kt
+++
@@ -1,7 +0,0 @@
-package com.appsanur.app.data.model
-
-data class NewsList (
-    val data: List<News> = arrayListOf(),
-    val length: Int = 0,
-    val status: Int = 0
-)
--- app/src/main/java/com/appsanur/app/ui/favorite/FavoriteViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.appsanur.app.ui.favorite
-
-import androidx.lifecycle.ViewModel
-
-class FavoriteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
